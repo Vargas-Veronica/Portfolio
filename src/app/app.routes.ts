6cdca6c9c4dd313638bc338d3befe26d8abd848b
Vargas-Veronica <vargas.veronica55@gmail.com>
2025-09-15 23:16:41 -0300

    estructura y tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  { path: 'projects', component: ProjectsComponent },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'about',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'about',
+  },
+];
